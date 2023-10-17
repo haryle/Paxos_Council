5ecde8567db6e4109ce73451aa6b6c5ea6b0f082
hson <lehoangsonsg@gmail.com>
2023-10-17 11:54:23 +1030

    feat: add project files

--- src/Main.java
+++ src/Main.java
@@ -1,18 +1,11 @@
+import org.javatuples.Pair;
+
 // Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
 // then press Enter. You can now see whitespace characters in your code.
 public class Main {
     public static void main(String[] args) {
-        // Press Alt+Enter with your caret at the highlighted text to see how
-        // IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
-
-        // Press Shift+F10 or click the green arrow button in the gutter to run the
-        // code.
-        for (int i = 1; i <= 5; i++) {
-
-            // Press Shift+F9 to start debugging your code. We have set one breakpoint
-            // for you, but you can always add more by pressing Ctrl+F8.
-            System.out.println("i = " + i);
-        }
+        Pair<Integer, Integer> first = Pair.with(10, 20);
+        Pair<Integer, Integer> second = Pair.with(10, 10);
+        System.out.println(first.compareTo(second));
     }
 }
