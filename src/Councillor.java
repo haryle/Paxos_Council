4eaf384bfa5f9d5d36059a236898bdad62f0291b
hson <lehoangsonsg@gmail.com>
2023-10-17 11:53:56 +1030

    feat: add roles

---
+++ src/Councillor.java
@@ -0,0 +1,7 @@
+public abstract class Councillor {
+    private final int councillorID;
+
+    public Councillor(int councillorID) {
+        this.councillorID = councillorID;
+    }
+}
---
+++ src/Proposer.java
@@ -0,0 +1,13 @@
+import org.javatuples.Pair;
+
+public class Proposer {
+
+//    public String prepare(int proposerID) {
+//    }
+//
+//    public void receive(Pair<Integer, Integer> timestamp, int value) {
+//
+//    }
+//
+//    public String accept()
+}
