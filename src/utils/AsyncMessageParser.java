f2a17421349ca02d1f54afc2f8f321174a629647
hson <lehoangsonsg@gmail.com>
2023-10-21 01:02:06 +1030

    feat: add async mesage parser

---
+++ src/utils/AsyncMessageParser.java
@@ -0,0 +1,31 @@
+package utils;
+
+public class AsyncMessageParser {
+    public StringBuilder builder;
+
+
+    public AsyncMessageParser() {
+        builder = new StringBuilder();
+    }
+
+    public String toString() {
+        return builder.toString();
+    }
+
+    public String[] append(String str) {
+        String msg = builder.append(str).toString();
+        if (msg.contains(Message.delimiter)) {
+            String[] retVal = msg.split(Message.delimiter);
+            if (!msg.endsWith(Message.delimiter)) {
+                String[] actualRetVal = new String[retVal.length - 1];
+                System.arraycopy(retVal, 0, actualRetVal, 0, retVal.length - 1);
+                builder = new StringBuilder(retVal[retVal.length - 1]);
+                return actualRetVal;
+            } else {
+                builder.setLength(0);
+                return retVal;
+            }
+        } else
+            return null;
+    }
+}
---
+++ tests/utils/AsyncMessageParserTest.java
@@ -0,0 +1,50 @@
+package utils;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class AsyncMessageParserTest {
+
+    AsyncMessageParser parser;
+
+    @BeforeEach
+    void setup(){
+        parser = new AsyncMessageParser();
+    }
+    @Test
+    void appendStringEndsWithDelimiterOneTest() {
+        String[] output = parser.append("0:0:PREPARE:0:0;");
+        assertEquals(output.length, 1);
+        assertEquals(output[0], "0:0:PREPARE:0:0");
+
+        output = parser.append("0:0:PREPARE:0:1;");
+        assertEquals(output.length, 1);
+        assertEquals(output[0], "0:0:PREPARE:0:1");
+    }
+
+    @Test
+    void appendStringEndsWithDelimiterMultipleTest() {
+        String[] output = parser.append("0:0:PREPARE:0:0;0:0:PREPARE:0:1;");
+        assertEquals(output.length, 2);
+        assertEquals(output[0], "0:0:PREPARE:0:0");
+        assertEquals(output[1], "0:0:PREPARE:0:1");
+
+        output = parser.append("0:0:PREPARE:0:2;");
+        assertEquals(output.length, 1);
+        assertEquals(output[0], "0:0:PREPARE:0:2");
+    }
+
+    @Test
+    void appendStringDoesNotEndsWithDelimiterMultipleTest() {
+        String[] output = parser.append("0:0:PREPARE:0:0;0:0:PREPARE:0:1;0:0:PREPARE:0:");
+        assertEquals(output.length, 2);
+        assertEquals(output[0], "0:0:PREPARE:0:0");
+        assertEquals(output[1], "0:0:PREPARE:0:1");
+
+        output = parser.append("2;");
+        assertEquals(output.length, 1);
+        assertEquals(output[0], "0:0:PREPARE:0:2");
+    }
+}
