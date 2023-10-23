3e4514a198306699d1aee73fd469f5bb7903e8aa
hoangson <lehoangsonsg@gmail.com>
2023-10-23 11:21:42 +1030

    feat: add comments for acceptor councillor

--- src/AcceptorCouncillor.java
+++ src/AcceptorCouncillor.java
@@ -3,41 +3,62 @@ import utils.helpers.Message;
 
 import java.io.IOException;
 
+/**
+ * Councillor who is an acceptor. AcceptorCouncillor only reacts to incoming
+ * message from proposer.
+ */
 public class AcceptorCouncillor extends AsyncClientConnection {
     private final int councillorID;
 
     private final Acceptor acceptorHandler;
 
+    /**
+     * Connect to the Broadcast Server which acts as the central communicator
+     * <p>
+     * Also initiate councillor ID
+     *
+     * @param host:         broadcast server hostname
+     * @param port:         broadcast server port
+     * @param councillorID: councillor ID
+     * @throws IOException if error during setup
+     */
     public AcceptorCouncillor(String host, int port, int councillorID) throws IOException {
         super(host, port);
         this.councillorID = councillorID;
         acceptorHandler = new Acceptor();
     }
 
+    /**
+     * Before starting, sends a connect message to the registry,
+     * then starts the background read and write threads
+     *
+     * @throws IOException if failures during sending
+     */
     public void start() throws IOException {
         send(Message.connect(councillorID));
         super.start();
     }
 
+    /**
+     * Handle incoming propose and prepare messages
+     * <p>
+     * Generate a reply for sender and send to the central registry which relays
+     * the message back to sender. If the message is directing itself, don't relay
+     *
+     * @param message parametrized message
+     * @throws IOException if there is any sending error
+     */
     @Override
     public void handleMessage(Message message) throws IOException {
+        Message reply = null;
         if (message.type.equalsIgnoreCase("PREPARE"))
-            handlePrepare(message);
+            reply = acceptorHandler.handleMessage(message);
         else if (message.type.equalsIgnoreCase("PROPOSE"))
-            handlePropose(message);
+            reply = acceptorHandler.handleMessage(message);
         else
             logger.info("Acceptor receives invalid message: " + message);
-    }
-
-    public void handlePropose(Message message) throws IOException {
-        Message reply = acceptorHandler.handleMessage(message);
-        if (reply.to != councillorID)
-            send(reply);
-    }
-
-    public void handlePrepare(Message message) throws IOException {
-        Message reply = acceptorHandler.handleMessage(message);
-        if (reply.to != councillorID)
+        // Does not send reply if the reply is addressing itself or is null
+        if (reply != null && reply.to != councillorID)
             send(reply);
     }
 }
